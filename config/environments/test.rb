6adbbd254916c28f98ac315fa929081fe7d7a883
Agilistry Studio <agilistrystudio@Agilistry-Studios-Mac-mini.local>
2010-05-10 12:28:32 -0700

    Added fixjour

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,3 +33,4 @@ config.gem "cucumber", :version => '0.7.2', :lib => false
 config.gem "cucumber-rails", :version => '0.3.1', :lib => false
 config.gem "webrat", :version => '0.7.1', :lib => false
 config.gem "database_cleaner", :version => '0.5.2', :lib => false
+config.gem 'fixjour', :version => "0.4.2", :lib => false
