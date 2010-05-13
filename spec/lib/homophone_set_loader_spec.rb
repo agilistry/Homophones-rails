380e9a58bac0f20fae5b20ec6ae22529ade7ec7e
Agilistry Studio <agilistrystudio@Agilistry-Studios-Mac-mini-4.local>
2010-05-13 15:21:20 -0700

    improved tests for homophone set loader for cases when it returns an empty homlist [DE, KO]

--- spec/lib/homophone_set_loader_spec.rb
+++ spec/lib/homophone_set_loader_spec.rb
@@ -1,35 +1,35 @@
 require 'spec_helper'
 
-describe HomophoneSetLoader do
-  before(:each) do
-    @loader = HomophoneSetLoader.new
-  end
-  
-  def make_json_string(top_level_key)
-    template = <<-EOD
-                {
-                  "#{top_level_key}": []
-                  }
-                EOD
-  end
+describe HomophoneSetLoader, "returns an empty homlist" do
 
   def should_return_empty_homlist(input)
-    loaded_homsets = @loader.create_homsets_from_json_string(input)
+    loader = HomophoneSetLoader.new
+    loaded_homsets = loader.create_homsets_from_json_string(input)
     loaded_homsets.length.should == 0
   end
-  
-  it "returns an empty homlist if JSON file is empty" do
-    loaded_homsets = @loader.create_homsets_from_json_string("")
-    loaded_homsets.length.should == 0
+
+  it "if JSON string is empty" do
+    should_return_empty_homlist("")
   end
-  
-  it "returns an empty homlist if JSON contains no homlist" do
-    a_json_string_with_no_homlist = make_json_string("PUNKROCK")
-    should_return_empty_homlist(a_json_string_with_no_homlist)
-   end
-   
-   it "returns an empty homlist if JSON contains an empty homlist" do
-     a_json_string_with_an_empty_homlist = make_json_string("HOMLIST")
-     should_return_empty_homlist(a_json_string_with_an_empty_homlist)
-    end
+
+  it "if JSON contains no homlist" do
+    input = {:PUNKROCK => []}.to_json
+    should_return_empty_homlist(input)
+  end
+
+  it "if JSON contains an empty homlist" do
+    input = {:HOMLIST => []}.to_json
+    should_return_empty_homlist(input)
+  end
+
+  it "if JSON contains no homsets" do
+    input = {:HOMLIST => [{}]}.to_json
+    should_return_empty_homlist(input)
+  end
+
+  it "if JSON contains only empty homsets" do
+    input = {:HOMLIST => [{:PHONES => []}]}.to_json
+    should_return_empty_homlist(input)
+  end
+
 end
