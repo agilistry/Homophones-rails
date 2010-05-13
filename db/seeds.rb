4dccd5b446ac605b71c189539fb23af6c08c6c92
Agilistry Studio <agilistrystudio@Agilistry-Studios-Mac-mini-4.local>
2010-05-13 11:45:13 -0700

    rewrote loading homophone sets from json file to exclude duplicates. no tests yet. [DE, KO, BJ]

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#   
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Major.create(:name => 'Daley', :city => cities.first)
-HomophoneSet.transaction do
-  hom_list = JSON.load(File.read(File.join(Rails.root, "homlist.txt")))['HOMLIST'].
-    map {|list| list['PHONES']}.compact.
-    each do |homset|
-      hs = HomophoneSet.new
-      homset.each do |hom|
-        hs.homophones << Homophone.create(:name => hom['KEY'], :definition => hom['DEF'])
-      end
-      hs.save!
-    end
-end
+
+HomophoneSet.delete_all
+Homophone.delete_all
+loader = HomophoneSetLoader.new
+loader.load_from_file 'homlist.txt'
+loader.save
---
+++ lib/homophone_set_loader.rb
@@ -0,0 +1,43 @@
+class HomophoneSetLoader
+  attr_accessor :raw_sets
+  
+  def load_raw_contents(filename)
+    file = File.join(Rails.root, filename);
+    raw_contents = File.read(file)
+  end
+
+  def create_homsets_from_json(raw_contents)
+    contents = JSON.load(raw_contents)
+    hs = {}
+    raw_sets = contents['HOMLIST'].each do |list|
+      puts list["PHONES"]
+      phones = list["PHONES"]
+      next if phones.blank?
+   
+      key = phones.collect{|i| i["KEY"]}.sort.join("-")
+
+      hs[key] =  phones.collect{|phone| {:name => phone['KEY'], 
+        :definition => phone['DEF']}
+        } unless hs.has_key? key 
+    end
+    hs
+  end
+
+  def load_from_file(filename)
+    raw_contents = load_raw_contents(filename)
+    @raw_sets = create_homsets_from_json(raw_contents)
+  end
+
+  def save
+    @raw_sets.each do |key, phones|
+      HomophoneSet.transaction do
+        hs = HomophoneSet.new
+        phones.each do |phone|
+          hs.homophones << Homophone.new(phone)
+        end
+        hs.save!
+      end
+    end  
+  end
+
+end
