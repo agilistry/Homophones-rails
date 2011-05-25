4038f9458d090f2e4d4fe995c8e74f867b2adbb9
Agilistry Studio <agilistrystudio@homophone.local>
2011-05-25 16:24:03 -0700

    error messages

--- Gemfile
+++ Gemfile
@@ -7,8 +7,6 @@ gem 'devise', '1.1.3'
 
 gem 'sqlite3-ruby', '1.2.5', :require => 'sqlite3'
 
-gem 'dynamic_form'
-
 group :development, :test, :ci, :cucumber do
   gem 'database_cleaner'
   gem 'rspec-rails', '>= 1.3.2'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def error_messages_for(resource)
+    return if resource.errors.blank?
+
+    items = resource.errors.full_messages.map do |error|
+      content_tag(:li, error)
+    end    
+    raw content_tag(:ul, raw(items))
+  end
 end
