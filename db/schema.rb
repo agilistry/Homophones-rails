c9e25836b0803bee3c636cac9a2403401f0efd78
Brendon Murphy <xternal1+github@gmail.com>
2011-05-25 16:10:52 -0700

    Fix the old devise fields migration to check if columns exist.  Dynamic devise migrators cause issues between versions.

--- db/schema.rb
+++ db/schema.rb
@@ -47,7 +47,6 @@ ActiveRecord::Schema.define(:version => 20110525185145) do
     t.string   "email",                                    :default => "", :null => false
     t.string   "encrypted_password",        :limit => 128, :default => "", :null => false
     t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer  "sign_in_count",                            :default => 0
     t.datetime "current_sign_in_at"
