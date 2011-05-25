35af6397f303ee0975af2f1d6e817c8574d54b67
Agilistry Studio <agilistrystudio@homophone.local>
2011-05-25 12:30:24 -0700

    log to STDOUT in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Homophones::Application.configure do
   # config.log_level = :debug
 
   # Use a different logger for distributed setups
-  # config.logger = SyslogLogger.new
+  config.logger = Logger.new(STDOUT)
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
