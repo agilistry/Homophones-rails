1055eb5436342fa9a1bc514d93f444203f5d1b05
Brendon Murphy <xternal1+github@gmail.com>
2011-05-25 17:19:55 -0700

    Adding new schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110525185145) do
+ActiveRecord::Schema.define(:version => 20110526001635) do
 
   create_table "homophone_sets", :force => true do |t|
     t.datetime "created_at"
