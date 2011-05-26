adb877cca64c5a07474144d93cdc0a85e4ad3962
Alex Bepple <alex@bepple.de>
2011-05-26 10:45:02 -0700

    Updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(:version => 20110526001635) do
     t.string   "email",                                    :default => "", :null => false
     t.string   "encrypted_password",        :limit => 128, :default => "", :null => false
     t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer  "sign_in_count",                            :default => 0
     t.datetime "current_sign_in_at"
