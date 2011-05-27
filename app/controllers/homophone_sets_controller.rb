73054a2a0161d9ea4783bf20309a0d873b0b154c
Elisabeth Hendrickson <esh@qualitytree.com>
2011-05-26 17:06:39 -0700

    removing search and updated specs

--- app/controllers/homophone_sets_controller.rb
+++ app/controllers/homophone_sets_controller.rb
@@ -3,18 +3,4 @@ class HomophoneSetsController < ApplicationController
   def index
   end
 
-  def search
-    @grouped_hom_list = HomophoneSearch.search_by_homophone params[:search_term]
-  end
-
-end
-
-class HomophoneSearch
-  class << self
-    def search_by_homophone(search_string)
-      matched_homophones = Homophone.find(:all, :conditions => {:name => search_string})
-      matched_sets = HomophoneSet.find_and_return_phones(matched_homophones.collect(&:homophone_set_id))
-      HomophoneSetGrouper.by_first_letter(matched_sets)
-    end
-  end
 end
--- spec/lib/homophone_set_grouper_spec.rb
+++ spec/lib/homophone_set_grouper_spec.rb
@@ -2,31 +2,23 @@ require 'spec_helper'
 
 describe HomophoneSetGrouper, 'by_first_letter' do
   it "returns a hash of grouped homophone sets" do
-    a_set = [OpenStruct.new(:name => 'a'), OpenStruct.new(:name => 'eh')]
-    ads_set = [OpenStruct.new(:name => 'ads'), OpenStruct.new(:name => 'adds')]
-    bask_set = [OpenStruct.new(:name => 'bask'), OpenStruct.new(:name => 'basque')]
+    a_set = HomophoneSet.new :from => %w(a eh)
+    ads_set = HomophoneSet.new :from => %w(ads adds)
+    bask_set = HomophoneSet.new :from => %w(bask basque)
     grouped = HomophoneSetGrouper.by_first_letter [a_set, ads_set, bask_set]
     grouped['a'].should == [a_set, ads_set]
     grouped['b'].should == [bask_set]
   end
 
   it "is case-insensitive" do
-    a_set = [OpenStruct.new(:name => 'a'), OpenStruct.new(:name => 'eh')]
-    ads_set = [OpenStruct.new(:name => 'Ads'), OpenStruct.new(:name => 'adds')]
+    a_set = HomophoneSet.new :from => %w(a eh)
+    ads_set = HomophoneSet.new :from => %w(Ads adds)
     grouped = HomophoneSetGrouper.by_first_letter [a_set, ads_set]
     grouped['a'].should == [a_set, ads_set]
   end
 
   it "orders the keys alphabetically" do
-    sets = [
-      [OpenStruct.new(:name => 'b')],
-      [OpenStruct.new(:name => 'a')],
-      [OpenStruct.new(:name => 'f')],
-      [OpenStruct.new(:name => 'r')],
-      [OpenStruct.new(:name => 'm')],
-      [OpenStruct.new(:name => 'z')],
-      [OpenStruct.new(:name => 'e')],
-    ]
+    sets = %w(b a f r m z e).map {|c| HomophoneSet.new :from => c }
     ordered_set = HomophoneSetGrouper.by_first_letter sets
     ordered_set.should be_a(ActiveSupport::OrderedHash)
     ordered_set.keys.should == %w(a b e f m r z)
